f9e168a486ff76235acced923d91184ec0745afc
Daniela <danielagp96@gmail.com>
2023-04-11 17:57:10 -0600

    adding debbuger options to node

--- pages/api/avo/[id].js
+++ pages/api/avo/[id].js
@@ -2,6 +2,8 @@ import db from '../../../database/db'
 
 const avoById = async (req,res) => { 
     const entry = await db.getById(req.query.id)
+    res.statusCode=200
+    res.setHeader('Content-type','application/json')
     res.end(JSON.stringify({data:entry}))
 }
 
