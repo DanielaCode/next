68b1ad55ce642c03749e28d724bd251dbd5f7bbf
Daniela <danielagp96@gmail.com>
2023-04-11 20:02:28 -0600

    using fetch to consume  the api from homepage component and painting it on the dom

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1630ca6849b274b7"; }
+/******/ 		__webpack_require__.h = function() { return "6c43dae87ce99528"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import NavBar from '../components/NavBar/NavBar'
 
 const index = () => {
+  const [productList,setProductList]=useState([])
+useEffect(()=>{
+  window
+  .fetch("/api/avo")
+  .then(data=>data.json())
+  .then(({data})=>setProductList(data))
+},[])
   return (
     <div>
         <NavBar/>
-        index
+        {productList.map(item=><div key={item.id}>{item.name}</div>)}
     </div>
   )
 }
